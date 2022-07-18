cde6a73c15e266da621d7b1d2fa70fa424a093b3
Entropie <pietro.ciconali@outlook.com>
2022-07-19 00:57:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,22 +4,20 @@ from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import matplotlib.pyplot as plt
 
-def costruisci_toro(r, R, r_densityF, theta_densityF):
+def costruisci_toro(r, R, r_density, theta_density):
     x, y = symbols("x y")
     f = sqrt(r**2-(sqrt(x**2+y**2)-R)**2)
     # r è il raggio della circonferenza che ruota attorno all'asse z
     # R è il raggio della circonferenza che passa per tutti i centri delle circonferenze di raggio r
 
-    r_density = r_densityF + 2
-    theta_density = theta_densityF + 1
 
     xyz = np.zeros((r_density, theta_density, 3)) # array dei punti del toro
 
-    for i in range(r_densityF):
-        for j in range(theta_densityF):
+    for i in range(r_density):
+        for j in range(theta_density):
 
-            r_temp = R - r + i * (2 * r) / r_density
-            theta_temp = 2 * pi * j / theta_density
+            r_temp = R - r + i * (2 * r) / (r_density - 1)
+            theta_temp = 2 * pi * j / (theta_density - 1)
 
             xp = float(r_temp * cos(theta_temp))
             yp = float(r_temp * sin(theta_temp))
@@ -30,42 +28,7 @@ def costruisci_toro(r, R, r_densityF, theta_densityF):
             xyz[i][j] = [xp, yp, zp] # Poi li sbatto dentro l'array
 
 
-        xyz[i][theta_densityF] = xyz[i][0]
 
-
-    second_to_last_row = np.zeros((theta_density, 3))
-    for j in range(theta_densityF):
-
-        r_temp = R - r + (r_densityF + 1) / r_density * 2 * r
-        theta_temp = 2 * pi * j / theta_density
-
-        xp = float(r_temp * cos(theta_temp))
-        yp = float(r_temp * sin(theta_temp))
-        zp = f.subs([(x, xp), (y, yp)])
-        if (not zp.is_real) or zp < 10 ** (-4):  # Messo per bilanciare l'appossimazione
-            zp = 0
-
-        second_to_last_row[j] = [xp, yp, zp]
-
-    second_to_last_row[theta_densityF] = second_to_last_row[0]
-
-
-    last_row = np.zeros((theta_density, 3))
-
-    for j in range(theta_densityF):
-        r_temp = R + r
-        theta_temp = 2 * pi * j / theta_density
-
-        xp = float(r_temp * cos(theta_temp))
-        yp = float(r_temp * sin(theta_temp))
-        last_row[j] = [xp, yp, 0]
-
-    last_row[theta_densityF] = last_row[0]
-
-    print(last_row)
-    xyz[r_densityF] = second_to_last_row
-    xyz[r_densityF + 1] = last_row
-    print(xyz)
 
     numero_sottomatrici = (theta_density-1)*(r_density- 1)
     sottomatrici = np.zeros((numero_sottomatrici, 4, 3))
@@ -109,4 +72,4 @@ def costruisci_toro(r, R, r_densityF, theta_densityF):
 
 
 if __name__ == '__main__':
-    costruisci_toro(10, 30, 21, 21)
+    costruisci_toro(10, 30, 20, 20)
